1b33f547c3bb3bc8bae6e60bdfea8b5cc640551b
arilybaert <arilybae@arteveldehs.be>
2020-12-26 15:37:05 +0100

    added static search/genre page

--- local-tunes-client/src/app/pages/Search.js
+++ local-tunes-client/src/app/pages/Search.js
@@ -6,6 +6,54 @@ const Search = () => {
         <div>
             <HeaderContainer/>
             <Player/>
+            <div className="o-masonry">
+                <div className="o-mansonry-1 o-masonryCommon">
+                    <div className="m-masonry">
+                        <p className="a-masonry">Indie</p>
+                    </div>
+                </div>
+                <div className="o-mansonry-2 o-masonryCommon">
+                    <div className="m-masonry">
+                        <p className="a-masonry">Indie</p>
+                    </div>
+                </div>
+                <div className="o-mansonry-3 o-masonryCommon">
+                    <div className="m-masonry">
+                        <p className="a-masonry">New Wave</p>
+                    </div>
+                </div>
+                <div className="o-mansonry-4 o-masonryCommon">
+                    <div className="m-masonry">
+                        <p className="a-masonry">Alternative</p>
+                    </div>
+                </div>
+                <div className="o-mansonry-5 o-masonryCommon">
+                    <div className="m-masonry">
+                        <p className="a-masonry">Metal</p>
+                    </div>
+                </div>
+                <div className="o-mansonry-6 o-masonryCommon">
+                    <div className="m-masonry">
+                        <p className="a-masonry">Hip Hop</p>
+                    </div>
+                </div>
+                <div className="o-mansonry-7 o-masonryCommon">
+                    <div className="m-masonry">
+                        <p className="a-masonry">Classic</p>
+                    </div>
+                </div>
+                <div className="o-mansonry-8 o-masonryCommon">
+                    <div className="m-masonry">
+                        <p className="a-masonry">Rock</p>
+                    </div>
+                </div>
+                <div className="o-mansonry-9 o-masonryCommon">
+                    <div className="m-masonry">
+                        <p className="a-masonry">Punk</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
