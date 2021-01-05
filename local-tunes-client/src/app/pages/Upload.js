e0dbc82b7645e3172fbfc297fc109e4f84c5ef29
arilybaert <arilybae@arteveldehs.be>
2021-01-05 15:55:58 +0100

    now you can upload multiple songs at once

--- local-tunes-client/src/app/pages/Upload.js
+++ local-tunes-client/src/app/pages/Upload.js
@@ -7,6 +7,8 @@ const Upload = () => {
     const [form, setForm] = useState({
         title: "",
     });
+    const [ uploadedSongs, setUploadedSongs ] = useState([]);
+
     const handleChange = (event) => {
         const value = event.target.value;
         setForm({
@@ -21,17 +23,39 @@ const Upload = () => {
         mode: 'no-cors',
         headers: { 
             'Authorization': `Bearer ${localStorage.getItem('login')}`,
-            'Content-Type': 'multipart/form-data',
-            'Accept': '*/*',
+            // 'Content-Type': 'multipart/form-data',
+            // 'Accept': '*/*',
         },
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const musicfile = document.getElementById("a-music");
-        const formData = new FormData();
-        formData.append("file", musicfile.files[0]);
-        formData.append("title", form.title);
+
+
+        Array.from(musicfile.files).forEach((f) => { 
+            const formData = new FormData();
+
+            // console.log(f.data.data.id);
+            formData.set("file", f);
+
+            axios.post(
+                apiUrl,
+                formData,
+                config
+            ).then((res) => {
+                setUploadedSongs(uploadedSongs.push(res.data.id));
+                console.log(uploadedSongs);
+            }).catch((err) => {
+                console.log(err.response.data.message);
+            })
+        });
+
+
+
+        
+        // console.log(musicfile.files);
+        // formData.append("title", form.title);
 
        /*
         @TODO upload form in one movement
@@ -40,15 +64,7 @@ const Upload = () => {
         const formData = new FormData(uploadForm);
         */
 
-        axios.post(
-            apiUrl,
-            formData,
-            config
-        ).then((res) => {
-            console.log(res);
-        }).catch((err) => {
-            console.log(err.response.data.message);
-        })
+
 
     }
 
@@ -70,18 +86,27 @@ const Upload = () => {
 
                     <form className="m-form">
                         <label htmlFor="musicfile" className="a-authTextLabel">Music file</label>
-                        <input type="file" name="musicfile" className="a-authTextInput" id="a-music"></input>
-                        <label htmlFor="coverart" className="a-authTextLabel">Cover-art</label>
-                        <input type="text" name="coverart" className="a-authTextInput"></input>
-                        <label htmlFor="title" className="a-authTextLabel" id="a-title">Title</label>
-                        <input type="text" name="title" className="a-authTextInput" onChange={handleChange}></input>
-                        <label htmlFor="artist" className="a-authTextLabel">Artist</label>
-                        <input type="text" name="artist" className="a-authTextInput" ></input>
-                        <label htmlFor="album" className="a-authTextLabel">Album</label>
-                        <input type="text" name="album" className="a-authTextInput"></input>
+                        <input type="file" name="musicfile" className="a-authTextInput" id="a-music" multiple></input>
+ 
+                        {/* <label htmlFor="title" className="a-authTextLabel" id="a-title">Title</label>
+                        <input type="text" name="title" className="a-authTextInput" onChange={handleChange}></input> */}
 
                         <button type="submit" className="a-authButton" onClick={handleSubmit}>Upload</button>
 
+                    </form>
+
+                    <form className="m-form">
+                        <label htmlFor="coverart" className="a-authTextLabel">Cover-art</label>
+                        <input type="file" name="coverart" className="a-authTextInput"></input>
+                    </form>
+
+                    <form className="m-form">
+                    <label htmlFor="artist" className="a-authTextLabel">Artist</label>
+                        <input type="text" name="artist" className="a-authTextInput" ></input>
+                        <label htmlFor="album" className="a-authTextLabel">Album</label>
+                        <input type="text" name="album" className="a-authTextInput"></input>
+                        <input type="hidden" name="cover-art"></input>
+                        <input type="hidden" name="songs"></input>
                     </form>
                 </div>
             </div>
